19b2011f4c3a752c4739f3f17e42190c2cd3c1d4
Schogan <lrschanbacher@gmail.com>
2018-01-31 18:13:22 -0600

    Added comments

--- CIS2013_Week3_Quiz1.cpp
+++ CIS2013_Week3_Quiz1.cpp
@@ -8,14 +8,18 @@ int main(){
 	char action;
 	bool keep_running = true;
 	
+	// While keeps program running as long as bool keep_running remains true
 	while (keep_running){
+		// request for first and second numbers
 		cout << "Please enter your first number: " << endl;
 		cin >> first_number;
 		cout << "Please enter your second number: " << endl;
 		cin >> second_number;
+		// Request for what action to take
 		cout << "What action would you like to perform? Please enter either + or *: " << endl;
 		cin >> action;
 		
+		// If statements for each possible action
 		if (action == '+'){
 			cout << "Your answer is: ";
 			cout << first_number + second_number << endl;
